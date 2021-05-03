aa6001507f17d64cabf9c905909d1cc122904658
苏祥 <60846352+MiracleStep@users.noreply.github.com>
2021-05-03 13:22:35 +0800

    Update DevController.java

--- src/main/java/com/mirac/item/controller/DevController.java
+++ src/main/java/com/mirac/item/controller/DevController.java
@@ -15,23 +15,29 @@ import java.nio.charset.StandardCharsets;
 @RequestMapping("/dev")
 public class DevController {
 
+    private static Boolean dev1 = false;
+    private static Boolean dev2 = false;
+
     @GetMapping("/control")
     public ResponseEntity<Void> controlBydevId(@RequestParam("devId1")Boolean devId1,@RequestParam("devId2")Boolean devId2) throws IOException {
         if(devId1==null || devId2==null || SocketThread.outputStream == null){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
 
-        if(devId1){
-            SocketThread.outputStream.write("2ON ".getBytes(StandardCharsets.UTF_8));
-        }else {
-            SocketThread.outputStream.write("2OFF ".getBytes(StandardCharsets.UTF_8));
+        if(devId1&&devId1!=dev1){
+            SocketThread.outputStream.write("a".getBytes(StandardCharsets.UTF_8));
+            dev1 = devId1;
+        }else if(!devId1&&devId1!=dev1) {
+            SocketThread.outputStream.write("b".getBytes(StandardCharsets.UTF_8));
+            dev1 = devId1;
         }
-
-
-        if(devId2){
-            SocketThread.outputStream.write("2ON ".getBytes(StandardCharsets.UTF_8));
-        }else {
-            SocketThread.outputStream.write("2OFF ".getBytes(StandardCharsets.UTF_8));
+        
+        if(devId2&&devId2!=dev2){
+            dev2 = devId2;
+            SocketThread.outputStream.write("c".getBytes(StandardCharsets.UTF_8));
+        }else if(!devId2&&devId2!=dev2){
+            dev2 = devId2;
+            SocketThread.outputStream.write("d".getBytes(StandardCharsets.UTF_8));
         }
 
         return ResponseEntity.ok().build();
